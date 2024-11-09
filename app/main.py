e7c33b731e9d63beed16e63313495e62a4d30e26
Mohamed Saeed bin Omar <131279191+MohamedSaeed-dev@users.noreply.github.com>
2024-11-09 16:17:27 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["http://localhost:3000","https://youtube-downloader-mohasa.netlify.app/"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
